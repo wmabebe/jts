d9b2df6aabb061f206edff86f782be4e385d3321
winki <mathiaswinkler89@gmail.com>
2014-10-16 13:25:16 +0200

    removed debug line

--- src/main/java/ch/bfh/ti/jts/gui/Window.java
+++ src/main/java/ch/bfh/ti/jts/gui/Window.java
@@ -170,7 +170,6 @@ public class Window {
             public void componentResized(final ComponentEvent e) {
                 windoww = panel.getWidth();
                 windowh = panel.getHeight();
-                System.out.println(windowh);
             }
         });
         frame.addKeyListener(new KeyAdapter() {
